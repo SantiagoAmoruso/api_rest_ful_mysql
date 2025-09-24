59b2ede24e7284a500739c559c4bf51c1909d7ed
Amoruso Santiago <amoruso.santiago@tecnica7.edu.ar>
2025-09-24 19:14:51 -0300

    MySQL

--- index.js
+++ index.js
@@ -3,10 +3,24 @@ const app = express();
 const port = 3000;
 const __dirname = import.meta.dirname;
 
+app.use(express.json())
 
-//raiz
-app.get('/', (req, res) =>{
-    res.send('api, rest ful con datos desde un array')
+app.get ('/', (req, res) => {
+    res.send('Api_rest_ful_MySQL')
 })
 
-app.listen(PORT, console.log('servidor http://Localhost: ${PORT}'))
+app.post('/',(req,res) => {
+    res.send('post')
+})
+
+app.put('/',(req,res) => {
+    res.send('put')
+})
+
+app.delete('/', (req,res) => {
+    res.send('delete')
+})
+
+app.use((req,res) => {res.status(404),send('pagina inexistente')})
+
+app.listen(port, console.log('servidor http://Localhost: ${PORT}'))
